524c87aa3869a16c48e19c04b7bd58566d2c52b8
ananyagupta1114 <ananyagupta1114@gmail.com>
2018-08-06 21:02:59 +0000

    Done.

--- silly_string.rb
+++ silly_string.rb
@@ -1,11 +1,13 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+ your_favorite_word = "Avocado"
 
 puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
+
+puts"Avocado".upcase
 
 favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
 
@@ -17,19 +19,19 @@ phrase = "IM NOT SHOUTING"
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
-puts lowercased_phrase
+puts "IM NOT SHOUTING".downcase
 
 # Challenge 4: Count how many letters are in that big_word
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word.length # Put a . after big_word and use a String method.
 
 puts "There are #{letter_count} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "mom" + "wow" # Use String concatenation to add "wow" and "mom" to big_word
 
 new_letter_count = bigger_word.size
 
@@ -37,9 +39,9 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".capitalize # Put a . after the string to capitalize it properly.
 
-puts proper_sentence
+puts proper_sentence.capitalize
 
 # Challenge 7: String Switcheroo
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
