cfa98ca6c7b94ee1cd16ee4c36a418e250de8476
Monica Para <monikapara0@gmail.com>
2018-08-06 20:37:55 +0000

    Done.

---
+++ ip.rb
@@ -0,0 +1,16 @@
+def my_fav_things
+
+fav_animal = "turtle"
+fav_place = "Chicago"
+fav_artist = "Shakira"
+fav_fruit = "Blueberries"
+
+  puts "I love #{fav_animal}!"
+  puts "I love #{fav_place}!"
+  puts "I love #{fav_artist}!"
+  puts "My favorite fruit is #{fav_fruit}"
+  
+end
+
+#envoking or calling the function
+my_fav_things
