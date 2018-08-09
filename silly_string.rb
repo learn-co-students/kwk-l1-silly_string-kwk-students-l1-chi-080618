986ec6fdb9024345e88b16019edb421887f5d166
Rowaelkhateeb <rowaelkhateeb2001@gmail.com>
2018-08-09 16:37:15 +0000

    Done.

--- silly_string.rb
+++ silly_string.rb
@@ -1,21 +1,19 @@
-
 # Challenge 1: Fill this string in with your favorite word
+puts "Pizza"
 
-your_favorite_word = ""
-
-puts "My favorite word is #{your_favorite_word}"
+# puts "pizza" #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
+puts "pizza".upcase
+# favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
-
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts "Pizza".upcase
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
-
-phrase = "IM NOT SHOUTING"
-
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+puts "pizza".downcase
+# phrase = "IM NOT SHOUTING"
+puts "IM NOT SHOUTING".downcase
+# lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
 puts lowercased_phrase
 
@@ -24,12 +22,14 @@ puts lowercased_phrase
 big_word = "supercalifragilisticexpialidocious"
 
 letter_count = big_word # Put a . after big_word and use a String method.
-
+puts"supercalifragilisticexpialidocious".length
 puts "There are #{letter_count} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
 bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+puts "big_word". +mom
+puts "supercalifragilisticexpialidocious". mom + wow
 
 new_letter_count = bigger_word.size
 
@@ -38,7 +38,7 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 # Challenge 6: Capitalize this sentence properly
 
 proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
-
+puts "i really like programming."capitalize
 puts proper_sentence
 
 # Challenge 7: String Switcheroo
